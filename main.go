111f2ea309d71ff2e9cb2516df10f64bc06e9f6d
srtkkou <srtkkou@outlook.jp>
2024-05-26 23:15:25 +0900

    [feat] Added code to read env variables.
    * 環境変数を読み込むコードを追加。

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package prxmail
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func AppMain(gitRevision string) (code int) {
-	fmt.Println("Hello world")
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error=%+v\n", err)
+		return -1
+	}
+	fmt.Printf("host=%s,port=%s,from=%s,password=%s\n",
+		os.Getenv("HOST"), os.Getenv("PORT"),
+		os.Getenv("FROM"), os.Getenv("PASSWORD"))
 	return 0
 }
