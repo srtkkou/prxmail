ac4587d255512bdd158b33366ff8f4c0e60cfa81
srtkkou <srtkkou@outlook.jp>
2024-05-27 12:48:34 +0900

    [feat] Added code to send email over TLS.
    * メールをTLS+SMTP経由で送信するコードを追加。

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package prxmail
 
 import (
+	"crypto/tls"
 	"errors"
+	"fmt"
+	"io"
+	"net/smtp"
 	"os"
 
 	"github.com/goark/errs"
@@ -11,6 +15,20 @@ import (
 var (
 	// envファイルロードエラー
 	ErrMainDotenvLoad = errors.New("main.ErrMainDotendLoad")
+	// TLSダイヤルエラー
+	ErrMainTlsDial = errors.New("main.ErrMainTlsDial")
+	// SMTPクライアント初期化エラー
+	ErrMainSmtpNewClient = errors.New("main.ErrMainSmtpNewClient")
+	// SMTPエラー
+	ErrMainSmtpAuth = errors.New("main.ErrMainSmtpAuth")
+	// SMTP MAILコマンドエラー
+	ErrMainSmtpMail = errors.New("main.ErrMainSmtpMail")
+	// SMTP RCPTコマンドエラー
+	ErrMainSmtpRcpt = errors.New("main.ErrMainSmtpRcpt")
+	// SMTP DATAコマンドエラー
+	ErrMainSmtpData = errors.New("main.ErrMainSmtpData")
+	// SMTP 書き込みエラー
+	ErrMainSmtpWrite = errors.New("main.ErrMainSmtpWrite")
 )
 
 func AppMain(gitRevision string) (code int) {
@@ -48,12 +66,70 @@ func AppMain(gitRevision string) (code int) {
 		Logger.Error().Err(err).Msg(logMsg)
 		return -1
 	}
-	message, err := mail.Message()
-	if err != nil {
+	// メールの送信
+	if err := Send(host, port, username, password, mail); err != nil {
 		Logger.Error().Err(err).Msg(logMsg)
 		return -1
 	}
-	// メールの送信
-	Logger.Info().Str("Message", message).Msg(logMsg)
 	return 0
+}
+
+func Send(
+	host string, port string,
+	username string, password string,
+	mail *Mail,
+) error {
+	logMsg := "prxmail.main.Send()"
+	// TLS認証準備
+	tlsServer := fmt.Sprintf("%s:%s", host, port)
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         host,
+	}
+	// TLS通信開始
+	tlsConn, err := tls.Dial("tcp", tlsServer, tlsConf)
+	if err != nil {
+		return errs.Wrap(ErrMainTlsDial, errs.WithCause(err),
+			errs.WithContext("server", tlsServer))
+	}
+	defer tlsConn.Close()
+	// SMTP通信開始
+	client, err := smtp.NewClient(tlsConn, host)
+	if err != nil {
+		return errs.Wrap(ErrMainSmtpNewClient, errs.WithCause(err))
+	}
+	defer client.Quit()
+	// 認証の実行
+	auth := smtp.PlainAuth("", username, password, host)
+	if err = client.Auth(auth); err != nil {
+		return errs.Wrap(ErrMainSmtpAuth, errs.WithCause(err))
+	}
+	// MAILコマンドの実行
+	if err = client.Mail(mail.From().String()); err != nil {
+		return errs.Wrap(ErrMainSmtpMail, errs.WithCause(err))
+	}
+	// RCPTコマンドの実行
+	for _, recipient := range mail.Recipients() {
+		if err = client.Rcpt(recipient.String()); err != nil {
+			return errs.Wrap(ErrMainSmtpRcpt, errs.WithCause(err))
+		}
+	}
+	// DATAコマンドの実行
+	w, err := client.Data()
+	if err != nil {
+		return errs.Wrap(ErrMainSmtpData, errs.WithCause(err))
+	}
+	defer w.Close()
+	writer := io.MultiWriter(w, os.Stdout)
+	// メッセージの取得
+	message, err := mail.Message()
+	if err != nil {
+		return err
+	}
+	Logger.Info().Str("Message", message).Msg(logMsg)
+	// メッセージの書き込み
+	if _, err = writer.Write([]byte(message)); err != nil {
+		return errs.Wrap(ErrMainSmtpWrite, errs.WithCause(err))
+	}
+	return nil
 }
